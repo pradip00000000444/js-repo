5de5bd041e96c279b6320c15d1d57dee12a8f498
pradip Bhat <pradipbhat150@gmail.com>
2026-08-30 21:22:36 +0545

    using multiline comment

--- 01-const,var.js
+++ 01-const,var.js
@@ -3,7 +3,7 @@ let address = "dillibazar,pipalbot"
 let emailid = "pradipbhat150@gmail.com"
 let state = "jharkhand"
 /*prefer to use let and neglect var because issue in blovk scope and function scope. here scope means the ( "{ 
-}" ). */
+}" ). */ 
 
 
-console.table([accountid,address,emailid,state])
+console.table([accountid,address,emailid,state]) 

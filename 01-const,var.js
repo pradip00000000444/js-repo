a7e1679c3a78d7135af054869e48a73d09fa86f7
pradip Bhat <pradipbhat150@gmail.com>
2026-08-30 21:18:38 +0545

    learned about the variables,let and constant.

---
+++ 01-const,var.js
@@ -0,0 +1,9 @@
+const accountid = 14444
+let address = "dillibazar,pipalbot"
+let emailid = "pradipbhat150@gmail.com"
+let state = "jharkhand"
+/*prefer to use let and neglect var because issue in blovk scope and function scope. here scope means the ( "{ 
+}" ). */
+
+
+console.table([accountid,address,emailid,state])

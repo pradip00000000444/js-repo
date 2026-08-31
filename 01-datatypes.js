5e3121515768721ee3a50b2c7f81065fc5933b32
pradip Bhat <pradipbhat150@gmail.com>
2026-08-31 08:19:45 +0545

    lerning about ythe data types.

---
+++ 01-datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as  newer version. 
+
+// alter (3+3)  we are using nodejs, not browser. 
+
+console.log(5+4) 
+console.log("kamana")
+
+let name ="pradip"
+let age = 10
+let isloggedin = false 
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// bollean => true/false
+// null => standalone  value 
+// undefined => 
+// symbol => unQue
+// object 
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object 

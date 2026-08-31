b61dfe47a3da5052c505e0ff52d70a924c88dde9
pradip Bhat <pradipbhat150@gmail.com>
2026-08-31 15:37:10 +0545

    understanding about operations

--- 03-conversitionoperation;.js
+++ 03-conversitionoperation;.js
@@ -1,29 +1,50 @@
-let score = "pradip"
+// let score = "pradip"
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNnumber = Number(score)
-console.log(typeof valueInNnumber)
-console.log(valueInNnumber)
+// let valueInNnumber = Number(score)
+// console.log(typeof valueInNnumber)
+// console.log(valueInNnumber)
 
 // "33 => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isloggedin = 99
+// let isloggedin = 99
 
-let booleanisloggedin = Boolean(isloggedin)
-console.log(booleanisloggedin);
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
 
 // 1 => true; 0 => false 
 // "" => false 
 // "pradip" => true
 
-let somenumbers = null
-let Stringnumber = String(somenumbers)
-console.log(Stringnumber)                           //each of the data  types firt letter shgould be in uppercase and ; is optional we can write but its  not mandatory 
-console.log(typeof Stringnumber)
+// let somenumbers = null
+// let Stringnumber = String(somenumbers)
+// console.log(Stringnumber)                           //each of the data  types firt letter shgould be in uppercase and ; is optional we can write but its  not mandatory 
+// console.log(typeof Stringnumber)
 
 // ******************opearions**********************
 
-4
+// let value = 9
+// let negvalue = -(9)
+// // console.log(negvalue)
+//  let str1 = "pradip"
+//  let str2 = " bhat"
+//  let  str3 = str1+str2 
+//  console.log(str3)
+ 
+
+// console.log("1"+ 3)
+// console.log(1+"2")
+// console.log("1"+2+3)
+// console.log(1+2+"1")
+
+console.log(+true)
+console.log(+"") 
+ 
+let num1,num2,num3
+num1=num2=num3 = 2+2 
+let gamecounter =100 
+++gamecounter;       // here i have used prefix increment 
+console.log(gamecounter)

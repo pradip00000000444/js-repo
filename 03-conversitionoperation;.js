ea254d011935368dc27e08913957ed8dce64a560
pradip Bhat <pradipbhat150@gmail.com>
2026-08-31 08:55:12 +0545

    basic conversion operation is done.

---
+++ 03-conversitionoperation;.js
@@ -0,0 +1,25 @@
+let score = "pradip"
+
+console.log(typeof score)
+
+let valueInNnumber = Number(score)
+console.log(typeof valueInNnumber)
+console.log(valueInNnumber)
+
+// "33 => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggedin = 99
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+// 1 => true; 0 => false 
+// "" => false 
+// "pradip" => true
+
+let somenumbers = null
+let Stringnumber = String(somenumbers)
+console.log(Stringnumber)
+console.log(typeof Stringnumber)

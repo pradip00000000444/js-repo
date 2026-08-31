153cab550b00eeb0e72c4f9ee96668d9104fa68e
pradip Bhat <pradipbhat150@gmail.com>
2026-08-31 16:24:29 +0545

    comparison done

---
+++ 04-compraison.js
@@ -0,0 +1,17 @@
+// console.log(1>2)
+// console.log(1<2)
+// console.log(1>=2)
+// console.log(1<=2)
+// console .log (1!=2)
+
+// console.log("2">1)
+// console.log("02">1) // there is  no any predictable result in the comparison of two diffrent data types.
+ 
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null>=0)
+//here there is no predictable result because, when there is > and >=, the fist condition  is correct 
+// but in 2nd case == its false. because eQuility check (==) treat as empty and comprasion sign treat it as a number i.e 0.
+
+console.log("2" === 2) //here == checks they are eQual or not and converstion takes pace but 
+// when we use === then it also checks the datatype

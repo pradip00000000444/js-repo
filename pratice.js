be19f783a529335385e4f422c2c51afd23220607
pradip Bhat <pradipbhat150@gmail.com>
2026-09-01 21:43:02 +0545

    pratice of string is done

--- pratice.js
+++ pratice.js
@@ -1,4 +1,12 @@
-let marks = "harshit"
-let number = Number(marks)
-console.log(number)
-console.log(typeof number)
+// let marks = "harshit"
+// let number = Number(marks)
+// console.log(number)
+// console.log(typeof number) 
+ 
+const name = "pradip"
+const exerep = 12
+console.log(`hello my name is ${name} and exercise rep count is ${exerep}`) 
+
+const playername = String(`kamana+00`)
+console.log(playername.length)
+console.log(playername.charAt(1))

8577db8441bba881dd94ebd2c6c16b9686d00447
pradip Bhat <pradipbhat150@gmail.com>
2026-09-01 08:15:27 +0545

    done

--- 04-compraison.js
+++ 04-compraison.js
@@ -14,4 +14,4 @@
 // but in 2nd case == its false. because eQuility check (==) treat as empty and comprasion sign treat it as a number i.e 0.
 
 console.log("2" === 2) //here == checks they are eQual or not and converstion takes pace but 
-// when we use === then it also checks the datatype
+// when we use === then it also checks the datatype 

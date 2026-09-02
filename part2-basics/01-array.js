1264d3aef124a218ab7f69ce0d9a7c3882ae4885
pradip Bhat <pradipbhat150@gmail.com>
2026-09-02 16:22:37 +0545

    using array  methods

--- part2-basics/01-array.js
+++ part2-basics/01-array.js
@@ -1,6 +1,16 @@
 // ************array***********
 
-const myArray = [0,1,2,3,4,5,6]
+const myArray = [0,1,2,3,4,5]
 const myheros = ["ironman","spiderman","thor"]
-const mylove = new Arrayv(1,2,3,4,5)// 3 ways of declaring array 
-console.log(myArray[0])
+const mylove = new Array(1,2,3,4,5)// 3 ways of declaring array 
+console.log(myArray[1]);
+
+
+// array methods 
+// myArray.push(6)
+// myArray.push(7) // cont total array numbers 
+// myArray.pop() // remove added array value
+
+myArray.unshift(9) // adding number in the first of the array 
+console.log(myArray)
+
---
+++ part2-basics/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+const myheros = ["ironman","spiderman","thor"]
+const mylove = new Arrayv(1,2,3,4,5)// 3 ways of declaring array 

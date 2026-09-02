25896d1551c5af6c2a3bf2061f8f3197b7d90f05
pradip Bhat <pradipbhat150@gmail.com>
2026-09-02 15:18:10 +0545

    learning about the datye and times in js

---
+++ 06-datesinjs.js
@@ -0,0 +1,10 @@
+// dates
+
+let mydate = new Date() 
+console.log(mydate.toString()); // return the current date and time in string format 
+console.log(mydate.toDateString()); // return the current date in string format
+console.log(mydate.toLocaleDateString()); // return the current date in local string format
+console.log(typeof mydate); // return the type of mydate which is object
+
+let mycreatedate = new Date(2026, 8, 2)
+console.log(mycreatedate.toString()); // return the created date in string format

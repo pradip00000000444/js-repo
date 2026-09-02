92f78730ca0f3b8b5f5e3eb9e9f58970b73aa818
pradip Bhat <pradipbhat150@gmail.com>
2026-09-02 15:51:06 +0545

    date and time done

--- 06-datesinjs.js
+++ 06-datesinjs.js
@@ -1,10 +1,25 @@
 // dates
 
 let mydate = new Date() 
-console.log(mydate.toString()); // return the current date and time in string format 
-console.log(mydate.toDateString()); // return the current date in string format
-console.log(mydate.toLocaleDateString()); // return the current date in local string format
-console.log(typeof mydate); // return the type of mydate which is object
+// console.log(mydate.toString()); // return the current date and time in string format 
+// console.log(mydate.toDateString()); // return the current date in string format
+// console.log(mydate.toLocaleDateString()); // return the current date in local string format
+// console.log(typeof mydate); // return the type of mydate which is object
 
 let mycreatedate = new Date(2026, 8, 2)
-console.log(mycreatedate.toString()); // return the created date in string format
+// console.log(mycreatedate.toLocaleString()); // return the created date in string format
+
+let mybirthdate = new Date("2006-12-04")
+// console.log(mybirthdate.toLocaleString()); // return the created date in string format
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp); // return the current timestamp in milliseconds since January 1, 1970 
+// console.log(mycreatedate.getTime()); // return the created date object
+// console.log(Math.floor(Date.now() / 1000)); // return the current timestamp in seconds since January 1, 1970
+
+
+// let newdate = new Date()
+// console.log(newdate)
+// console.log(newdate.getMonth())
+// console.log(newdate.getDay())
+

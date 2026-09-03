57e2d2e1e8eaa40911e686fca810afdaba780990
pradip Bhat <pradipbhat150@gmail.com>
2026-09-03 09:33:45 +0545

    done

--- part2-basics/02-arraypart2.js
+++ part2-basics/02-arraypart2.js
@@ -25,4 +25,4 @@ let score1 =100
 let score2 =200
 let score3 =300
 
-console.log(Array.of(score1,score2,score3)) // 
+console.log(Array.of(score1,score2,score3)) // .of usd to make these iteams in one singke array.

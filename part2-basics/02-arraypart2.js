7fd227fa5a0e67c0a64955197540ed6b2619fcbf
pradip Bhat <pradipbhat150@gmail.com>
2026-09-03 08:59:14 +0545

    part  2of array

---
+++ part2-basics/02-arraypart2.js
@@ -0,0 +1,7 @@
+const marvel_heros = ["ironman", "spiderman", "thor"]
+const dc_heros = ["superman", "flash", "batman"]
+
+marvel_heros.push(dc_heros)
+console.log(marvel_heros)
+console.log(marvel_heros[3][2]) // accssing the valude of the third element. 
+

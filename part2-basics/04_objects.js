84c3d5b8b1d1103c37456dc5b68e39b00fd74824
pradip Bhat <pradipbhat150@gmail.com>
2026-09-06 08:37:34 +0545

    normal use of objects

---
+++ part2-basics/04_objects.js
@@ -0,0 +1,8 @@
+// const tinderuser = new Object() // singleton 
+const tinderuser = {} // non singleton 
+tinderuser.id = "123abc"
+tinderuser.name = "harshit"
+tinderuser.isLoggedIn = false
+// console.log(tinderuser)
+
+

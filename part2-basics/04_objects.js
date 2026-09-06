96fb83bd69e4564ed84d25f5d0589fbf44ac4cf0
pradip Bhat <pradipbhat150@gmail.com>
2026-09-06 09:12:34 +0545

    part 2 of objects done.

--- part2-basics/04_objects.js
+++ part2-basics/04_objects.js
@@ -5,4 +5,30 @@ tinderuser.name = "harshit"
 tinderuser.isLoggedIn = false
 // console.log(tinderuser)
 
+const pradipuser = {
+  email: "pradip000.com",
+  fullname: {
+    userfullname: {
+      firstname: "Pradip",
+      lastname: "Bhat"
+    }
+  }
+} 
+// objects in objects 
+// console.log(pradipuser.email,pradipuser.fullname)
 
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj4 = {5:"e", 6:"f"}
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign(obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+console.log(obj3)   // ... means spread it is used to comboine the objects or arrays
+
+console.log(Object.keys(tinderuser))  // coverts the objects into array into the 
+console.log(Object.values(tinderuser))
+console.log(Object.entries(tinderuser))
+
+console.log(tinderuser.hasOwnProperty('pradiphero')) // to checl whether number is present or not in the program.
+ 
